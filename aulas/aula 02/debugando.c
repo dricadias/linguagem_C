4aa219a2bd3b89341efe4d146356e6ac46d0fc7e
Andrielly Dias <andrielly.12dias.ad@gmail.com>
2024-07-03 19:24:11 +0100

    aula 02

--- aulas/aula 02/debugando.c
+++ aulas/aula 02/debugando.c
@@ -8,6 +8,11 @@ int main(int argc, char *argv[]) {
 	printf("Mensagem 3.\n");
 	printf("Mensagem 4.\n");
 	printf("Mensagem 5.\n");
+	printf("Mensagem 6.\n");
+	printf("Mensagem 7.\n");
+	printf("Mensagem 8.\n");
+	printf("Mensagem 9.\n");
+	printf("Mensagem 10.\n");
 	
 	system("pause");
 	
